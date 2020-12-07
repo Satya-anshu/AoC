c0ec7e20473ad9690b7428d54427426937c1717d
Satyaanshu <saty@microsoft.com>
2020-12-07 13:11:11 +0530

    AoC 2020 Day 7

---
+++ 2020/Day 7/day7.py
@@ -0,0 +1,69 @@
+lines = open('sample2.txt','r').readlines()
+bagMap = {}
+bagCountMap = {}
+shinygold = 'shiny gold'
+
+for line in lines:
+    bags = line.strip('\n').strip('.').split('contain')
+    ''' Left side => Outer Bag
+        Rest = Inner Bag'''
+    outer_bag = ' '.join(bags[0].split(' ')[:2])
+    outer_bag = outer_bag.rstrip()
+    inner_bags = bags[1].split(',')
+    inner_bag = []
+    bagDict = {}
+    for i in inner_bags:
+        i = i.lstrip().rstrip().strip('bag').strip('bags')
+        i = i.rstrip()
+        ib = ' '.join(i.split(' ')[1:])
+        if ib != 'other':
+            num_ib = int(i.split(' ')[0])
+            bagDict[ib] = num_ib
+            if ib not in bagMap:
+                bagMap[ib] = []
+            bagMap[ib].append(outer_bag)
+    bagCountMap[outer_bag] = bagDict
+
+'''
+Find shiny gold and count bags
+'''
+def findBags(bag, bagMap, bagSeen):
+    if bag in bagSeen:
+        return
+    elif bag not in bagMap:
+        '''Leaf node'''
+        bagSeen.add(bag)
+        return
+    bagSeen.add(bag)
+    for each_bag in bagMap[bag]:
+        findBags(each_bag,bagMap,bagSeen)
+    
+
+bagSeen = set()
+if shinygold not in bagMap:
+    print("Part1 Puzzle: ", 0)
+else:
+    for i in bagMap[shinygold]:
+        findBags(i, bagMap,bagSeen)
+    print("Part1 Puzzle: ",len(bagSeen))
+
+'''
+Find Count of Inner Bags
+'''
+def findInnerBags(key, bagCountMap):
+    go_in = False
+    count = 0
+    for key,value in bagCountMap[key].items():
+        go_in = True
+        val = value * findInnerBags(key,bagCountMap)
+        count += value + val
+    if go_in:
+        return count
+    else:
+        return 0
+
+count = 0
+for key,value in bagCountMap[shinygold].items():
+    count += value + value * findInnerBags(key,bagCountMap)
+
+print("Part2 Puzzle: ", count)

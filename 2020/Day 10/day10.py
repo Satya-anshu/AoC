35a47c8bc887926d024e10ed3a911fe6c5b1aebc
Satyaanshu <saty@microsoft.com>
2020-12-10 13:09:03 +0530

    AoC 2020 Day 10

--- 2020/Day 10/day10.py
+++ 2020/Day 10/day10.py
@@ -17,11 +17,7 @@ print("Part 1: ", diff1 * diff3)
 '''
 Tribonacci
 '''
-dp = [0 for i in range(10)]
-dp[0] = 1
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
+dp = [1,1,2,4,0,0,0,0,0,0]
 for i in range(4,len(dp)):
     dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 

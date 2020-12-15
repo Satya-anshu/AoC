34bcb392abae978564e6c90502d066bc0127ddb6
Satyaanshu <saty@microsoft.com>
2020-12-15 10:56:55 +0530

    AOC 2020 Day 15

---
+++ 2020/Day 15/day15-2.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+num = [1,20,8,12,0,14]
+spoken = defaultdict()
+for i in range(len(num)):
+    spoken[num[i]] = [i+1]
+
+for i in range(len(num),30000000):
+    if len(spoken[num[i-1]]) == 1:
+        num.append(0)
+        if 0 in spoken:
+            spoken[0].append(i+1)
+        else:
+            spoken[0] = [i+1]
+    else:
+        diff = spoken[num[i-1]][-1] - spoken[num[i-1]][-2]
+        num.append(diff)
+        if diff in spoken:
+            spoken[diff].append(i+1)
+        else:
+            spoken[diff] = [i+1]
+            
+print(num[-1])
+    

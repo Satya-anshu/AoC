de505a922d1b45e0af641bba8a8f6fb275ade78d
Satya-anshu <satyaanshu@Satyaanshus-MacBook-Pro.local>
2021-12-02 10:36:51 +0530

    AoC Day 2

---
+++ 2021/Day_2/2.py
@@ -0,0 +1,48 @@
+import os
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
+test_input = \
+'''forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+'''
+def solve1(lines):
+    x = 0
+    y = 0
+    for line in lines:
+        ins,val = line.split()
+        if "forward" in ins:
+            x += int(val)
+        elif "down" in ins:
+            y += int(val)
+        else:
+            y -= int(val)
+    print(x*y)
+
+def solve2(lines):
+    x = 0
+    y = 0
+    aim = 0
+    for line in lines:
+        ins,val = line.split()
+        if "forward" in ins:
+            x += int(val)
+            y += aim * int(val)
+        elif "down" in ins:
+            #y += int(val)
+            aim += int(val)
+        else:
+            aim -= int(val)
+    print(x*y)
+
+if __name__ == "__main__":
+    lines = open("input.txt","r").read().splitlines()
+    linesTest = list(test_input.splitlines())
+    solve1(lines)
+    solve2(lines)

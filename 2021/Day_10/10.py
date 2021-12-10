25ab2c01f68a910f194974aaffdbf8aa3344731d
Satyaanshu <satyaanshu@outlook.com>
2021-12-10 11:16:52 +0530

    AoC Day 10

---
+++ 2021/Day_10/10.py
@@ -0,0 +1,80 @@
+import os
+import copy
+import collections
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
+test_input = \
+'''[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+'''
+def solve_1(lines):
+    error_score = 0
+    for line in lines:
+        stk = []
+        for bkt in line:
+            if bkt in ('(','[','{','<'):
+                stk.append(bkt)
+            else:
+                if stk[-1] == '(' and bkt != ')' or stk[-1] == '{' and bkt != '}' or stk[-1] == '[' and bkt != ']' or stk[-1] == '<' and bkt != '>':
+                    if bkt == ')':
+                       error_score+=3
+                    elif bkt == ']':
+                        error_score+=57
+                    elif bkt == '}':
+                        error_score+=1197
+                    else:
+                        error_score+=25137
+                    break
+                else:
+                    stk.pop(-1)
+    print(error_score)
+
+def solve_2(lines):
+    complete_scores = []
+    for line in lines:
+        stk = []
+        error = False
+        for bkt in line:
+            if bkt in ('(','[','{','<'):
+                stk.append(bkt)
+            else:
+                if stk[-1] == '(' and bkt != ')' or stk[-1] == '{' and bkt != '}' or stk[-1] == '[' and bkt != ']' or stk[-1] == '<' and bkt != '>':
+                    error = True
+                    break
+                else:
+                    stk.pop(-1)
+        if error:
+            continue
+        score = 0
+        for rem_bkt in reversed(stk):
+            if rem_bkt == '(':
+                val = 1
+            elif rem_bkt == '[':
+                val = 2
+            elif rem_bkt == '{':
+                val = 3
+            else:
+                val = 4
+            score = score * 5 + val
+        complete_scores.append(score)
+    complete_scores.sort()
+    print(complete_scores)
+    print(complete_scores[len(complete_scores)//2])
+
+if __name__ == "__main__":
+    lines = open("input.txt","r").read().splitlines()
+    linesTest = test_input.splitlines()
+    solve_1(lines)
+    solve_2(lines)

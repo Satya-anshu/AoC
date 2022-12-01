9f83661c0299174335bd8e5082a746608f0e36fe
Satyaanshu <satyaanshu@outlook.com>
2022-12-01 12:10:28 +0530

    AoC Day01 C#

---
+++ 2022/Day_01/day01/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static List<long> GetTotal()
+    {
+        List<long> total = new List<long>();
+        long current_sum = 0;
+        using (StreamReader sr = new StreamReader("input.txt"))
+        {
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line != "")
+                {
+                    long num = long.Parse(line);
+                    current_sum += num;
+                }
+                else
+                {
+                    total.Add(current_sum);
+                    current_sum = 0;
+                }
+            }
+            total.Sort();
+            total.Reverse();
+            return total;
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        List<long> total = GetTotal();
+
+        Console.WriteLine("Part 1: " + total.First());
+        Console.WriteLine("Part 2: " + total.Take(3).Sum());
+    }
+}
